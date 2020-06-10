0f085236f6f9348e57a64f85ce03ea035938c5dd
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-10 13:15:40 +0900

    Created Path Class to connect the Local Path of User, and Created Another Main

--- My_First_Project/Data_Class/model.py
+++ My_First_Project/Data_Class/model.py
@@ -9,4 +9,46 @@ from Data_Class.Data_Set import DataSet
 from Data_Class.Class_DS_Net import DS_Net
 
 
+class Path:
+    # Divide Constant and String Variable
+    n_classes = 14
+    BATCH_SIZE = 64
 
+    # Make Variable that can available to put strings.
+    def __init__(self, n_classes):
+        self.N_CLASSES = n_classes
+        self.CKPT_PATH = ''
+        self.CLASS_NAMES = ['']
+        self.DATA_DIR = ''
+        self.TEST_IMAGE_LIST = ''
+
+    # Realizing how to use Tensor is really good I guess..
+    def Calculation(self):
+        normalize = transforms.Normalize(
+            [0.485, 0.456, 0.406],
+            [0.229, 0.224, 0.225])
+
+        transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.TenCrop(224),
+            transforms.Lambda(
+                lambda crops: torch.stack([
+                    transforms.ToTensor()(crop) for crop in crops
+                ])
+            ),
+            transforms.Lambda(
+                lambda crops: torch.stack([
+                    normalize(crop) for crop in crops
+                ])
+            )
+        ])
+
+
+if __name__ == "__main__":
+    path = Path(transforms)
+
+    def process(image_list=None, auroc=False):
+        if image_list is None:
+            image_list = path.TEST_IMAGE_LIST
+
+        test_dataset = DataSet
