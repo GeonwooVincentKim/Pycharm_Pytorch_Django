840e7bc542e6e56595f8f7a83319dbfe12421799
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-10 12:51:22 +0900

    Currently Make DS_Network Program

--- My_First_Project/Data_Class/model.py
+++ My_First_Project/Data_Class/model.py
@@ -6,4 +6,7 @@ import numpy
 from sklearn.metrics import roc_auc_score
 
 from Data_Class.Data_Set import DataSet
+from Data_Class.Class_DS_Net import DS_Net
+
+
 
---
+++ My_First_Project/auroc.py
@@ -0,0 +1,8 @@
+from Data_Class.model import process
+
+
+# Now, there is no process function,
+# But I'm going to create soon as I can
+# understand how process function works.
+if __name__ == "__main__":
+    process(None, True)
