0e120fd58fc24b4f05b5e1a2da65cbd07ad48ac8
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-19 21:58:48 +0900

    Learning about DenseNet..

--- My_First_Project/Data_Class/model.py
+++ My_First_Project/Data_Class/model.py
@@ -1,12 +1,12 @@
 import os
-import json
-import torch, torchvision.transforms as transforms
-import numpy
 
-from sklearn.metrics import roc_auc_score
+import torch
+import torch.utils.data
+import torch.backends.cudnn
+import torchvision.transforms as transforms
 
-from Data_Class.Data_Set import DataSet
 from Data_Class.Class_DS_Net import DS_Net
+from Data_Class.Data_Set import DataSet
 
 
 class Path:
@@ -18,10 +18,13 @@ class Path:
     # Make Variable that can available to put strings.
     def __init__(self, n_classes):
         self.N_CLASSES = n_classes
-        self.CKPT_PATH = ''
-        self.CLASS_NAMES = ['']
-        self.DATA_DIR = ''
-        self.TEST_IMAGE_LIST = ''
+        self.CKPT_PATH = 'E:/Django/Pytorch_Django/Local_Project/First/My_First_Project/Data_Class/model.pth.tar'
+        self.CLASS_NAMES = ['Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 'Nodule', 'Pneumonia',
+                            'Pneumothorax', 'Consolidation', 'Edema', 'Emphysema', 'Fibrosis', 'Pleural_Thickening',
+                            'Hernia']
+        self.DATA_DIR = 'E:/Django/Pytorch_Django/Local_Project/First/My_First_Project/Data_Images/Images'
+        self.TEST_IMAGE_LIST = 'E:/Django/Pytorch_Django/Local_Project/First/My_First_Project/Data_Images/labels' \
+                               '/short_test_list.txt '
 
     # Realizing how to use Tensor is really good I guess..
     def Calculation(self):
@@ -48,6 +51,7 @@ class Path:
 if __name__ == "__main__":
     path = Path(transforms)
 
+
     def process(image_list=None, auroc=False):
         if image_list is None:
             image_list = path.TEST_IMAGE_LIST
@@ -61,3 +65,26 @@ if __name__ == "__main__":
         )
 
         torch.backends.cudnn.benchmark = True
+
+        # Load and Initialize the Model
+        model = DS_Net(path.n_classes).cuda()
+        model = torch.nn.DataParallel(model, device_ids=[0])
+
+        if os.path.isfile(path.CKPT_PATH):
+            print("Loading CheckPoint...")
+            checkpoint = torch.load(path.CKPT_PATH)
+            model.load_state_dict(checkpoint['state_dict'])
+            print("Successfully Loaded Checkpoint")
+        else:
+            print("Could not find checkpoint")
+
+        test_loader = torch.utils.data.DataLoader(dataset=test_dataset)
+
+        # Initialize the Ground Truth and output Tensor.
+        gt = torch.FloatTensor()
+        gt = gt.cuda()
+        pred = torch.FloatTensor()
+        pred = pred.cuda()
+
+        # Switch to evaluate mode
+        model.eval()
